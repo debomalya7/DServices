e53dfec394b894b9edc67234dabd68ff2ba7c5c5
debomalya7 <ray.debmalya7@gmail.com>
2019-06-02 13:17:44 +0530

    Addressing the review comments.

--- src/main/java/com/debs/service/FirstJava.java
+++ src/main/java/com/debs/service/FirstJava.java
@@ -1,5 +1,9 @@
-package com.debs.Service;
+package com.debs.service;
 
 public class FirstJava {
+	
+	public static void main(String[] args) {
+		System.out.println("Main Method:Hello World");
+	}
 
 }
